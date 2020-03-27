ab5d378ebadc1673cf18b6ac7059b3f8a6ce4b87
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-03-27 22:05:05 +0800

    modified:   FirstApplication/.vs/FirstApplication/v16/.suo         modified:   FirstApplication/FirstApplication/Program.cs         new file:   FirstApplication/FirstApplication/bin/Debug/netcoreapp3.1/FirstApplication.deps.json         new file:   FirstApplication/FirstApplication/bin/Debug/netcoreapp3.1/FirstApplication.dll         new file:   FirstApplication/FirstApplication/bin/Debug/netcoreapp3.1/FirstApplication.exe         new file:   FirstApplication/FirstApplication/bin/Debug/netcoreapp3.1/FirstApplication.pdb         new file:   FirstApplication/FirstApplication/bin/Debug/netcoreapp3.1/FirstApplication.runtimeconfig.dev.json         new file:   FirstApplication/FirstApplication/bin/Debug/netcoreapp3.1/FirstApplication.runtimeconfig.json         new file:   FirstApplication/FirstApplication/obj/Debug/netcoreapp3.1/FirstApplication.csproj.CoreCompileInputs.cache         new file:   FirstApplication/FirstApplication/obj/Debug/netcoreapp3.1/FirstApplication.csproj.FileListAbsolute.txt         modified:   FirstApplication/FirstApplication/obj/Debug/netcoreapp3.1/FirstApplication.csprojAssemblyReference.cache         new file:   FirstApplication/FirstApplication/obj/Debug/netcoreapp3.1/FirstApplication.dll         new file:   FirstApplication/FirstApplication/obj/Debug/netcoreapp3.1/FirstApplication.exe         new file:   FirstApplication/FirstApplication/obj/Debug/netcoreapp3.1/FirstApplication.genruntimeconfig.cache         new file:   FirstApplication/FirstApplication/obj/Debug/netcoreapp3.1/FirstApplication.pdb

--- FirstApplication/FirstApplication/Program.cs
+++ FirstApplication/FirstApplication/Program.cs
@@ -7,6 +7,8 @@ namespace FirstApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
         }
     }
 }
